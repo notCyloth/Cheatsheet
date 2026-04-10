64ef55da28059535b949c3da9c63ccb3ee97e8d6
notCyloth <110560666+notCyloth@users.noreply.github.com>
2026-04-10 15:57:16 +0100

    Update enum.sh

--- scripts/enum.sh
+++ scripts/enum.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+# Argument/Error Handling Section
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+if [ $# -eq 0 ] || [ -z "$1" ];
+  then
+    echo "No arguments supplied. i.e. ./enum.sh [IP ADDRESS]"
+fi
+
+if [ $# -gt 1 ];
+  then
+    echo "Extra arguments detected. i.e. ./enum.sh [IP ADDRESS]"
+fi
+
+whoami
