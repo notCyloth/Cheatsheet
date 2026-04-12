c0cf1d2ddad8cdbdfae26d72477c9085a73cba2d
notCyloth <110560666+notCyloth@users.noreply.github.com>
2026-04-12 16:16:20 +0100

    Update enum.sh

--- scripts/enum.sh
+++ scripts/enum.sh
@@ -32,6 +32,8 @@ mkdir -p $1/nmap/top200
 mkdir -p $1/nmap/fastallport
 mkdir -p $1/nmap/slowallport
 mkdir -p $1/nmap/aggressiveTCP
+mkdir -p $1/nmap/fastUDP
+mkdir -p $1/nmap/regUDP
 mkdir -p $1/searchsploit
 
 echo "[*] Starting top200 port scan."
@@ -49,7 +51,7 @@ nmap -p$(cat $1/nmap/fastallports.csv) -A $1 -oA $1/nmap/aggressiveTCP/aggressiv
 echo "[!] aggressiveTCP scan complete. Results at: $(pwd)/$1/nmap/aggressiveTCP"
 
 echo "[*] Feeding aggressiveTCP to searchsploit..."
-searchsploit --nmap $1/nmap/aggressiveTCP/aggressiveTCP.xml > $1/searchsploit/searchsploit.results
+searchsploit --nmap $1/nmap/aggressiveTCP/aggressiveTCP.xml > $1/searchsploit/searchsploitTCP.results
 echo "[!] Searchsploit finished. Results at: $(pwd)/$1/searchsploit/searchsploitTCP.results"
 
 # START SLOWALLPORT SCAN
@@ -58,15 +60,37 @@ nmap -p- $1 -oA $1/nmap/slowallport/slowallport -Pn -n >/dev/null
 echo "[!] slowallport scan complete. Results at: $(pwd)/$1/nmap/slowallport"
 grep "Ports:" $1/nmap/slowallport/slowallport.gnmap | sed 's/.*Ports: //' | tr ',' '\n' | grep "/open/" | awk -F'/' '{gsub(/ /,"",$1); print $1}' | sort -n | uniq | paste -sd',' - > $1/nmap/slowallports.csv
 
-# DIFF SLOW vs FAST FULL PORT SCAN - IF DIFFERENT, FEED SLOWFULLPORTSCAN INTO NEW AGGRESSIVETCP
 echo "[*] Comparing slowallports.csv to fastallports.csv"
 
-# FEED AGGRESSIVETCP SCAN INTO SEARCHSPLOIT
+if read -n1 -d '' < <(diff $1/nmap/fastallports.csv $1/nmap/slowallports.csv); 
+  then 
+    echo "[!] Differences between slow and fast all port scans detected..."
+    echo "[*] Feeding slowallport scan into aggressiveTCP scan..."
+    nmap -p$(cat $1/nmap/slowallports.csv) -A $1 -oA $1/nmap/aggressiveTCP/aggressiveTCP >/dev/null
+    echo "[!] aggressiveTCP scan complete. Results at: $(pwd)/$1/nmap/aggressiveTCP"
+    echo "[*] Feeding new aggressiveTCP to searchsploit..."
+    searchsploit --nmap $1/nmap/aggressiveTCP/aggressiveTCP.xml > $1/searchsploit/searchsploitTCP.results
+    echo "[!] Searchsploit finished. Results at: $(pwd)/$1/searchsploit/searchsploitTCP.results"
+fi
 
-# RUN -F UDP SCAN
+echo "[*] Starting fastUDP scan."
+nmap -F -sU $1 -oA $1/nmap/fastUDP/fastUDP
+echo "[!] fastUDP scan complete. Results at: $(pwd)/$1/nmap/fastUDP"
+grep "Ports:" $1/nmap/fastUDP/fastUDP.gnmap | sed 's/.*Ports: //' | tr ',' '\n' | grep "/open/" | awk -F'/' '{gsub(/ /,"",$1); print $1}' | sort -n | uniq | paste -sd',' - > $1/nmap/fastUDPports.csv
 
-# RUN UDP PORT SCAN
+echo "[*] Feeding fastUDP to searchsploit..."
+searchsploit --nmap $1/nmap/fastUDP/fastUDP.xml > $1/searchsploit/searchsploitUDP.results
+echo "[!] Searchsploit finished. Results at: $(pwd)/$1/searchsploit/searchsploitUDP.results"
 
-# FEED UDP PORT SCAN INTO SEARCHSPLOIT
+echo "[*] Starting regUDP scan."
+nmap -sU $1 -oA $1/nmap/regUDP/regUDP
+echo "[!] regUDP scan complete. Results at: $(pwd)/$1/nmap/regUDP"
+grep "Ports:" $1/nmap/regUDP/regUDP.gnmap | sed 's/.*Ports: //' | tr ',' '\n' | grep "/open/" | awk -F'/' '{gsub(/ /,"",$1); print $1}' | sort -n | uniq | paste -sd',' - > $1/nmap/regUDPports.csv
 
-# RUN ALL UDP PORT SCAN
+if read -n1 -d '' < <(diff $1/nmap/regUDPports.csv $1/nmap/fastUDPports.csv); 
+  then 
+    echo "[!] Differences between regular and fast UDP port scans detected..."
+    echo "[*] Feeding new regUDP scan to searchsploit..."
+    searchsploit --nmap $1/nmap/regUDP/regUDP.xml > $1/searchsploit/searchsploitUDP.results
+    echo "[!] Searchsploit finished. Results at: $(pwd)/$1/searchsploit/searchsploitTCP.results"
+fi
